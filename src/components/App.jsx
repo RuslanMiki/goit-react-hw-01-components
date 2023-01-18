46a602211eaa1b6f1f4d977cf13c97154f179436
RuslanMiki <mikitchenko_ruslan@ukr.net>
2023-01-18 18:21:54 +0100

    finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,12 @@
 import Profile from './Profile/Profile';
+import FriendList from './FriendList/FriendList';
 import Statistics from './Statistics/Statistics';
+import TransactionHistory from './TransactionHistory/TransactionHistory';
 
 import user from './Profile/user.json';
 import data from './Statistics/data.json';
-
+import friends from './FriendList/friends.json';
+import transactions from './TransactionHistory/transactions.json';
 
 export const App = () => {
   return (
@@ -15,9 +18,9 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
-      <Statistics
-        title="Upload stats"
-        stats={data} />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </>
   );
 };
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,40 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import css from './Statistics.module.css'
+import css from './Statistics.module.css';
 import getRandomHexColor from 'utils/randomFunc';
 
-
-
-const Statistics = ({title, stats}) => {
-        const elements = stats.map(stat => {
-            return (
-                <li key={stat.id} className={css.item} style={{backgroundColor: getRandomHexColor()}}>
-                    <span className={css.label}>{stat.label}</span>
-                    <span className={css.percentage}>{stat.percentage}%</span>
-                </li>
-            )
-        })
+const Statistics = ({ title, stats }) => {
+  const elements = stats.map(stat => {
     return (
-        <section className={css.statistics}>
-             {title && <h2 className={css.title}>{title}</h2>}
-            <ul className={css.statList}>
-                {elements}
-               </ul>
-        </section>
-
-    )
-}
-    
+      <li
+        key={stat.id}
+        className={css.item}
+        style={{ backgroundColor: getRandomHexColor() }}
+      >
+        <span className={css.label}>{stat.label}</span>
+        <span className={css.percentage}>{stat.percentage}%</span>
+      </li>
+    );
+  });
+  return (
+    <section className={css.statistics}>
+      {title && <h2 className={css.title}>{title}</h2>}
+      <ul className={css.statList}>{elements}</ul>
+    </section>
+  );
+};
 
 Statistics.propTypes = {
-    title: PropTypes.string,
-    stats: PropTypes.arrayOf(
-        PropTypes.exact({
-            id:PropTypes.string.isRequired,
-            label:PropTypes.string.isRequired,
-            percentage:PropTypes.number.isRequired,
-        })
-    ).isRequired,
-}
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+};
 
-    export default Statistics
+export default Statistics;
